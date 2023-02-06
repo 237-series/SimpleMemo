443a8d33b56ee090d6b1d712a959ee0f98bc5df6
sglee237 <sglee237@gmail.com>
2023-02-06 13:53:49 +0900

    Initial Commit

---
+++ SimpleMemo/SimpleMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMemoApp.swift
+//  SimpleMemo
+//
+//  Created by sglee237 on 2023/02/06.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
